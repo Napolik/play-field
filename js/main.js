17dbc504bb8ac3aed85de48eacd1fc88b1184320
Napolik <rv.setra@gmail.com>
2022-07-15 16:21:08 +0300

    first eetempt to generate walls

--- js/main.js
+++ js/main.js
@@ -24,16 +24,20 @@ class PlayField extends HTMLElement {
                    [0,0,1,0], [0,0,1,0], [0,0,1,0], [0,0,1,0], [0,1,1,0]]];
 
     this.drawWalls();
+
+    console.log(this.randomBoolean());
+    this.generateWalls();
   }
 
   drawWalls() {
+    document.querySelector('.playfield').innerHTML = '';
+
     for (let y = 0; y < this.table.length; y++) {
       for (let i = 0; i < this.table[y].length; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
 
         for (let j = 0; j < this.table[y][i].length; j++) {
-
           switch (j) {
             case 0 :
               drawBorder(this.table[y][i][j], 'top');
@@ -58,7 +62,34 @@ class PlayField extends HTMLElement {
         }
       }
     }
+  }
 
+  randomBoolean() {
+    const randomBoolean = Math.random() < 0.5;
+    return randomBoolean;
+  }
+
+  generateWalls() {
+    let result = [];
+    let row = [];
+
+    for (let i = 0; i < this.table.length; i++) {
+
+      let cell = [];
+
+      for (let j = 0 ; j < 4; j++) {
+        const rand = this.randomBoolean();
+        cell.push(Number(rand));
+      }
+      row.push(cell);
+      console.log(row);
+      result.push(row);
+    }
+
+    //console.log(result);
+
+    this.table = result;
+    this.drawWalls();
   }
 
 }
